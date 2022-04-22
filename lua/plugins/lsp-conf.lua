8024811478922b100b30e0951a1fe3f43baefb2a
rifqoi <alfurqon.rifqi@gmail.com>
2022-04-23 01:11:53 +0700

    Tabnine taking too much memory

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -8,7 +8,7 @@ local source_mapping = {
 	buffer = "[Buffer]",
 	nvim_lsp = "[LSP]",
 	nvim_lua = "[Lua]",
-	cmp_tabnine = "[TN]",
+	-- cmp_tabnine = "[TN]",
 	path = "[Path]",
 }
 local has_words_before = function()
@@ -69,12 +69,12 @@ cmp.setup({
 		format = function(entry, vim_item)
 			vim_item.kind = lspkind.presets.default[vim_item.kind]
 			local menu = source_mapping[entry.source.name]
-			if entry.source.name == "cmp_tabnine" then
-				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-					menu = entry.completion_item.data.detail .. " " .. menu
-				end
-				vim_item.kind = ""
-			end
+			-- if entry.source.name == "cmp_tabnine" then
+			-- 	if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+			-- 		menu = entry.completion_item.data.detail .. " " .. menu
+			-- 	end
+			-- 	vim_item.kind = ""
+			-- end
 			vim_item.menu = menu
 			return vim_item
 		end,
@@ -83,7 +83,7 @@ cmp.setup({
 	sources = {
 		-- tabnine completion? yayaya
 		{ name = "nvim_lsp" },
-		{ name = "cmp_tabnine" },
+		-- { name = "cmp_tabnine" },
 		-- { name = 'vsnip' },
 		{ name = "luasnip" },
 		-- { name = 'ultisnips' },
@@ -93,14 +93,14 @@ cmp.setup({
 	},
 })
 
-local tabnine = require("cmp_tabnine.config")
-tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = "..",
-})
+-- local tabnine = require("cmp_tabnine.config")
+-- tabnine:setup({
+-- 	max_lines = 1000,
+-- 	max_num_results = 20,
+-- 	sort = true,
+-- 	run_on_every_keystroke = true,
+-- 	snippet_placeholder = "..",
+-- })
 
 -- LSP Config
 --
