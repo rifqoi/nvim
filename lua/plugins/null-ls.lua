bc36890cfbdb3780a73f24ff078406174bf27b49
rifqoi <alfurqon.rifqi@gmail.com>
2022-10-27 21:13:18 +0700

    gatau udah banyak

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -177,10 +177,10 @@ end
 
 local lsp_formatting = function(bufnr)
 	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
+		-- filter = function(client)
+		-- 	-- apply whatever logic you want (in this example, we'll only use null-ls)
+		-- 	return client.name == "null-ls"
+		-- end,
 		bufnr = bufnr,
 	})
 end
